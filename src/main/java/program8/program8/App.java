032c928a1f247f83fa5050e628b4c76e6d196115
Gurudev Kini <kinigurudev049@gmail.com>
2026-03-31 11:08:13 +0530

    Initial commit

---
+++ src/main/java/program8/program8/App.java
@@ -0,0 +1,13 @@
+package program8.program8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
